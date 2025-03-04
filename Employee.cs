402d70e813e945be35409bd5791e45a32b91b676
minahiltahir-whiz <24ntucsfl1263@student.ntu.edu.pk>
2025-03-04 16:45:25 +0500

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Employee     //Class
+    {
+        //Attributes/States/Varaible
+        public int Employee_ID;
+        public string Name;
+        public string Department;
+        public double Salary;
+
+        //Constructor
+        public Employee(int E_id, string N, string D, double S)
+        {
+            Employee_ID = E_id;
+            Name = N;
+            Department = D;
+            Salary = S;
+
+        }
+
+        //Behaviours/Methods
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("         Employee Details are as : ");
+            Console.WriteLine($"Employee ID: {Employee_ID}.");
+            Console.WriteLine($"-->Name: {Name}.");
+            Console.WriteLine($"-->Department: {Department}.");
+            Console.WriteLine($"-->Salary: ${Salary}.");
+
+        }
+
+        public void IncreaseSalary(double Amount)
+        {
+            if (Amount < 0)
+            {
+                Console.WriteLine("Invalid salary increase amount!");
+                return;
+            }
+            else
+            {
+                Salary = Salary + Amount;
+            }
+        }
+
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("     Employee Management System     ");
+            Employee employee_1 = new Employee(0101, "Ali Ahmed", "IT Department", 70000);
+            Employee employee_2 = new Employee(0102, "Muzamil Sohail", "Buisness Department", 52000);
+
+            //Employee Details
+            employee_1.DisplayEmployeeDetails();
+            employee_2.DisplayEmployeeDetails();
+
+            //After Icreasing Salary
+            employee_1.IncreaseSalary(1500);
+            employee_2.IncreaseSalary(3500);
+
+            //Details of Employee After Salary increase.
+            Console.WriteLine($"\n Details after Incresing Salary.");
+
+            employee_1.DisplayEmployeeDetails();
+            employee_2.DisplayEmployeeDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
