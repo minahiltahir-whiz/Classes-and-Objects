f3c635eefb4610a4eab14c8978bd3ee604cc9ced
minahiltahir-whiz <24ntucsfl1263@student.ntu.edu.pk>
2025-03-04 16:53:09 +0500

    added folder named by Book.cs

---
+++ Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Book
+    {
+        //Attributes/States/Varaible
+        public string Title;
+        public string Author;
+        public string ISBN;
+        public double Price;
+
+
+        //Behaviours/Methods
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("           Book Details are as : ");
+            Console.WriteLine($"-->Book Title: {Title}");
+            Console.WriteLine($"-->Book Author: {Author}");
+            Console.WriteLine($"-->Book ISBN: {ISBN}");
+            Console.WriteLine($"-->Price of Book: ${Price}.");
+
+        }
+
+        public void Discount(double percentage)
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                Console.WriteLine("Invalid discount percentage!");
+                return;
+            }
+            Price -= Price * (percentage / 100);
+        }
+
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("     Library Management System     ");
+            Book book_1 = new Book();
+            book_1.Title = "Professional C# 7 and .NET Core 2.0.";
+            book_1.Author = "Christian Nagel.";
+            book_1.ISBN = "9888554032.";
+            book_1.Price = 980;
+
+
+            Book book_2 = new Book();
+            book_2.Title = "C# 7.0 in a Nutshell.";
+            book_2.Author = "Joseph Albahari and Ben Albahari.";
+            book_2.ISBN = "00678787661.";
+            book_2.Price = 1035;
+
+            //Book Details
+            book_1.DisplayDetails();
+            book_2.DisplayDetails();
+
+            //Applying Discount 
+            book_1.Discount(10);
+            book_2.Discount(25);
+
+            //Details of Book After DIScount
+            Console.WriteLine($"\n Details after Applying Discount.");
+
+            book_1.DisplayDetails();
+            book_2.DisplayDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
